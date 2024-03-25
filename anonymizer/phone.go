d1273425967cb9a4cf82d591b4e48d73cd8a3c45
Ken Yap Wei Chun <wei.chun@pickupp.io>
2024-03-26 08:31:50 +0900

    feat: finish phone anonymizer

--- anonymizer/phone.go
+++ anonymizer/phone.go
@@ -1,7 +1,11 @@
 package anonymizer
 
 import (
+	"bytes"
 	"errors"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 // Simple usage:
@@ -37,6 +41,50 @@ func (pa *phoneAnonymizer) SetReplacement(newReplacement string) *phoneAnonymize
 }
 
 func (pa *phoneAnonymizer) Anonymize(s string) string {
-	// TODO: Return text with anonymized phone number
-	return ""
+	// early return if the lastDigits == 0 to reduce on useless computation below
+	if pa.lastDigits == 0 {
+		return s
+	}
+	phoneRegex := `\+\d{2}\s\d{3}\s\d{3}\s\d{3}`
+	regex := regexp.MustCompile(phoneRegex)
+
+	// find all phone number matches
+	matches := regex.FindAllStringIndex(s, -1)
+
+	// instantiate a buffer to store the ananonymized string and the replaced string
+	var buffer bytes.Buffer
+	lastIndex := 0
+
+	// Iterate over each phone number match
+	for _, match := range matches {
+		start := match[0]
+		end := match[1]
+
+		// appending the string before phone number to the current buffer
+		// i.e: (Lorem) +48 bla bla bla bla
+		buffer.WriteString(s[lastIndex:start])
+
+		// extract phone number from original string provided
+		phoneNumber := s[start:end]
+
+		// dynamically define the regex pattern to match the provided lastDigits
+		lastDigitsPattern := regexp.MustCompile(`\d{` + strconv.Itoa(pa.lastDigits) + `}$`)
+
+		// replace all mention with provided `replacement` based on the lastDigitsPattern
+		anonymizedPhoneNumber := lastDigitsPattern.ReplaceAllStringFunc(phoneNumber, func(match string) string {
+			return strings.Repeat(string(pa.replacement), len(match))
+		})
+
+		// append the produced anonymizedPhoneNumber into the buffer
+		buffer.WriteString(anonymizedPhoneNumber)
+
+		// update the last index to trace current phone number update
+		lastIndex = end
+	}
+
+	// append the suffix part of the original string back into buffer
+	buffer.WriteString(s[lastIndex:])
+
+	// return the entire stringified buffer as response
+	return buffer.String()
 }
