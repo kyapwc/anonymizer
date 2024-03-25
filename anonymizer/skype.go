a9ea6d2c8201f3574bd70716b8abe08ece2e3e2e
Ken Yap Wei Chun <wei.chun@pickupp.io>
2024-03-26 08:31:38 +0900

    feat: finish skype anonymizer

--- anonymizer/skype.go
+++ anonymizer/skype.go
@@ -1,5 +1,11 @@
 package anonymizer
 
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
 // Simple usage:
 // NewSkypeAnonymizer("#").Anonymize(`<a href="skype:loren?call"/>`) -> <a href="skype:#?call"/>
 
@@ -14,6 +20,27 @@ func NewSkypeAnonymizer(replacement string) *skypeAnonymizer {
 }
 
 func (sa *skypeAnonymizer) Anonymize(s string) string {
-	// TODO: Return text with anonymized skype
-	return ""
+	originalString := s
+	usernameRegex := `skype:\w*`
+	regex := regexp.MustCompile(usernameRegex)
+
+	// find first occurence
+	usernameMatches := regex.FindAllStringIndex(s, -1)
+	fmt.Println("matches", usernameMatches)
+	// Better call skype:MySkype or at least skype:YourAdmin
+
+	if len(usernameMatches) > 0 {
+		for _, match := range usernameMatches {
+			startIndex := match[0]
+			endIndex := match[1]
+
+			username := originalString[startIndex:endIndex]
+
+			parts := strings.Split(username, ":")
+
+			s = strings.Replace(s, parts[1], "#", 1)
+		}
+	}
+
+	return s
 }
